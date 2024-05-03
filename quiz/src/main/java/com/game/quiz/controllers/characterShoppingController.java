da67db9d7d2d8faa359a31bdaf17c609adec5da2
MedAmineChakroun <164560880+MedAmineChakroun@users.noreply.github.com>
2024-05-03 21:02:16 +0100

    Adding CharacterShopping class and updating its controller

--- quiz/src/main/java/com/game/quiz/controllers/characterShoppingController.java
+++ quiz/src/main/java/com/game/quiz/controllers/characterShoppingController.java
@@ -2,13 +2,13 @@ package com.game.quiz.controllers;
 
 
 import com.game.quiz.entities.character_shopping;
-import com.game.quiz.repositories.characterShoppingRepository;
+import com.game.quiz.services.characterShoppingService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.ClientInfoStatus;
+
 import java.util.List;
 
 @RestController
@@ -16,27 +16,26 @@ import java.util.List;
 public class characterShoppingController {
 
     @Autowired
-    private characterShoppingRepository characterShoppingRepository;
+    private characterShoppingService characterShoppingService;
 
     @GetMapping
     public List<character_shopping> getAllShoppingCharacters() {
-
-        return characterShoppingRepository.findAll();
+        return characterShoppingService.getAllShoppingCharacters();
     }
+
     //get charactershopping by id
     @GetMapping("/{id}")
     public character_shopping getShoppingCharacterById(@PathVariable Long id) {
-
-        return characterShoppingRepository.findById(id).orElse(null);
+        return characterShoppingService.getShoppingCharacterById(id);
     }
 
     @PostMapping("/add")
     public character_shopping addShoppingCharacter(@RequestBody character_shopping characterShopping) {
-
-        return characterShoppingRepository.save(characterShopping);
+        return characterShoppingService.addShoppingCharacter(characterShopping);
     }
+
     @GetMapping("/byPlayer/{playerId}")
     public List<character_shopping> getShoppingsByPlayer(@PathVariable Long playerId) {
-        return characterShoppingRepository.findByPlayer_IdPlayer(playerId);
+        return characterShoppingService.getShoppingsByPlayer(playerId);
     }
 }
